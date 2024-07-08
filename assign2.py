c19739d8fbe6065bb7ed2987679b8c490e2ce243
koguls <kogulkogul4@gmail.com>
2024-07-08 10:22:41 +0530

    day2

---
+++ assign2.py
@@ -0,0 +1,8 @@
+#get the input from the user and print the output
+
+u_name =input("enter the user name:")
+m_id =input("your mail id :")
+ph = input("enter the phone number :")
+print("user name :",u_name)
+print("Email :",m_id)
+print("ph :",ph)
